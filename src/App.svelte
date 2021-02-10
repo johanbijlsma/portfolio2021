8437f257825e558a84c903a7f5965f57be20d28d
Johan Bijlsma <johan.bijlsma24@gmail.com>
2021-02-10 18:34:39 +0100

    init commit, started new project with Svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Header from './Header.svelte';
+export let name;
+</script>
+<Header />
+     <div class="container">
+
+<main>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem quos quaerat quod non cum distinctio accusamus consequuntur veniam fugiat. Placeat cupiditate officia rerum id, voluptate illo consequuntur quo error. Voluptatibus?
+</main>
+</div>
+<style type="text/scss">
+
+
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    let name = 'Johan Bijlsma';
+    let subline = 'Front-end developer... | Blogger... | Dad... | Musician... | Coffee lover...';
+</script>
+    <div class="container">
+
+<main>
+    <div class="hero">
+        <h1>{name}</h1>
+        <p>{subline}</p>
+    </div>
+</main>
+</div>
+<style type="text/scss">
+
+</style>
